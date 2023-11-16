dcf6fe70910a98a28bb4d658b61d01c070b6d866
Alejandra Villagomez Flores <micka_seido_19@hotmail.com>
2023-11-15 21:21:22 -0400

    examen

---
+++ alejandraV/PRIMER_PARCIAL/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Examen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            ClaseArray A = new ClaseArray(5);
+            A.ArrayEnteros[0] = 5;
+            A.ArrayEnteros[1] = 13;
+            A.ArrayEnteros[2] = 37;
+            A.ArrayEnteros[3] = 1;
+            A.ArrayEnteros[4] = 9;
+
+            A.SumaArrayEnteros();
+
+        }
+    }
+}
